675b3b493b3e40ac3fdd679479bd9dd9bddebbbe
matt <matt@claritum.com>
2022-09-07 19:21:37 +0100

    Make classes final

--- src/Signer/Signer.php
+++ src/Signer/Signer.php
@@ -29,14 +29,12 @@ use const OPENSSL_ALGO_SHA256;
 /**
  * @see \KynxTest\Laminas\Dkim\Signer\SignerTest
  */
-class Signer
+final class Signer
 {
     /**
      * All configurable params.
-     *
-     * @var array
      */
-    private $params = [
+    private array $params = [
         // optional params having a default value set
         'v' => '1',
         'a' => 'rsa-sha256',
@@ -49,17 +47,13 @@ class Signer
 
     /**
      * Empty DKIM header.
-     *
-     * @var Dkim
      */
-    private $emptyDkimHeader;
+    private Dkim $emptyDkimHeader;
 
     /**
      * Canonized headers.
-     *
-     * @var string
      */
-    private $canonizedHeaders;
+    private string $canonizedHeaders = '';
 
     /**
      * The private key being used.
@@ -110,11 +104,9 @@ class Signer
     /**
      * Set Dkim param.
      *
-     * @param string $key
-     * @param string $value
      * @throws Exception
      */
-    public function setParam($key, $value): self
+    public function setParam(string $key, string $value): self
     {
         if (! array_key_exists($key, $this->getParams())) {
             throw new Exception("Invalid param '$key' given.");
